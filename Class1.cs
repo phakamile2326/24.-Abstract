78e16794b11b3e18e88a65c480f8b13845c195d1
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-10 14:58:16 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstract
+{
+    internal class Corsa:Vehicle 
+    {
+        public int sparewheels = 1;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//vehicle car1= new vehicle();// this is not possible as parent class abstract: to increase security
+            Corsa car2  = new Corsa();
+
+            Console.WriteLine(car2.wheels);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
